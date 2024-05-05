a80fda451e08fc681cb090e9039d9393ca466b6b
Ollie-Moss <ollie-moss@live.nmit.ac.nz>
2024-05-05 16:41:50 +1200

    Added holepunching kinda

--- Scripts/HolePunchingClient.cs
+++ Scripts/HolePunchingClient.cs
@@ -9,113 +9,122 @@ using System.Runtime.CompilerServices;
 
 public partial class HolePunchingClient : Node2D
 {
-    [Signal]
-    public delegate void HolePunchedEventHandler();
+	[Signal]
+	public delegate void HolePunchedEventHandler();
+	
+	[Signal]
+	public delegate void HostStartedEventHandler();
+	
+	private bool _clientRunning = false;
+	private bool _isHost;
 
-    private bool _clientRunning = false;
+	public UdpClient client;
 
-    public UdpClient client;
+	private const string _hostRequest = "hr";
+	private const string _peerPunching = "pp";
+	private const string _holePunch = "hp";
 
-    private const string _hostRequest = "hr";
-    private const string _peerPunching = "pp";
-    private const string _holePunch = "hp";
+	[Export]
+	private string ServerIP = "127.0.0.1";
+	[Export]
+	private int ServerPort = 25565;
 
-    [Export]
-    private string ServerIP = "127.0.0.1";
-    [Export]
-    private int ServerPort = 25565;
+	// endpoint where server is listening
+	private IPEndPoint serverEP;
 
-    // endpoint where server is listening
-    private IPEndPoint serverEP;
+	public void StartClient(bool isHost)
+	{
+		_isHost = isHost;
+		serverEP = new IPEndPoint(IPAddress.Parse(ServerIP), ServerPort);
+		client = new UdpClient();
 
-    public void StartClient(bool isHost)
-    {
-        serverEP = new IPEndPoint(IPAddress.Parse(ServerIP), ServerPort);
-        client = new UdpClient();
+		//client.Connect(serverEP);
 
-        //client.Connect(serverEP);
-
-        _clientRunning = true;
-
-
-        if (isHost)
-        {
-            SendBytes("hr:PLEASE", serverEP);
-        }
-        else
-        {
-            SendBytes("cr:AAAA", serverEP);
-        }
-
-        while (_clientRunning)
-        {
-            var data = client.Receive(ref serverEP);
-
-            GD.Print($"receive data from {serverEP.ToString()}");
-
-            string[] packet = Encoding.ASCII.GetString(data).Split(":");
-            HandlePacket(packet);
-        }
-
-    }
-
-    // Handle incoming udp packet
-    public void HandlePacket(string[] packet)
-    {
-        switch (packet[0])
-        {
-            case _holePunch:
-                HolePunch(packet);
-                return;
-            case _hostRequest:
-                HostRequest(packet);
-                return;
-            case _peerPunching:
-                PeerPunching(packet);
-                return;
-            default:
-                Console.WriteLine("Invalid packet recieved from: {0}", serverEP.ToString());
-                return;
-        }
+		_clientRunning = true;
 
 
-        /*Console.WriteLine("Message received from {0}:", Remote.ToString());
-        Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));*/
-    }
+		if (isHost)
+		{
+			SendBytes("hr:PLEASE", serverEP);
+		}
+		else
+		{
+			SendBytes("cr:AAAA", serverEP);
+		}
 
-    // Is server alive
-    public void PeerPunching(string[] packet)
-    {
-        EmitSignal(SignalName.HolePunched);
-        CloseClient();
-    }
+		while (_clientRunning)
+		{
+			var data = client.Receive(ref serverEP);
 
-    public void HostRequest(string[] packet)
-    {
-        if (packet[1] == "OK")
-        {
-            GD.Print("Lobby Registered Created");
-            CloseClient();
-        }
-    }
+			GD.Print($"receive data from {serverEP.ToString()}");
 
-    public void HolePunch(string[] packet)
-    {
-        SendBytes("pp:Punching", new IPEndPoint(IPAddress.Parse(packet[1]), int.Parse(packet[2])));
-    }
+			string[] packet = Encoding.ASCII.GetString(data).Split(":");
+			HandlePacket(packet);
+		}
 
-    public void SendBytes(string data, IPEndPoint endpoint)
-    {
-        byte[] byteArray = Encoding.UTF8.GetBytes(data);
-        Console.WriteLine($"Sending data to: {endpoint.ToString()}");
-        client.Send(byteArray, endpoint);
-    }
+	}
 
-    public void CloseClient()
-    {
-        client.Close();
-        _clientRunning = false;
-    }
+	// Handle incoming udp packet
+	public void HandlePacket(string[] packet)
+	{
+		switch (packet[0])
+		{
+			case _holePunch:
+				HolePunch(packet);
+				return;
+			case _hostRequest:
+				HostRequest(packet);
+				return;
+			case _peerPunching:
+				PeerPunching(packet);
+				return;
+			default:
+				Console.WriteLine("Invalid packet recieved from: {0}", serverEP.ToString());
+				return;
+		}
+
+
+		/*Console.WriteLine("Message received from {0}:", Remote.ToString());
+		Console.WriteLine(Encoding.ASCII.GetString(data, 0, recv));*/
+	}
+
+	// Is server alive
+	public void PeerPunching(string[] packet)
+	{
+		if(!_isHost)
+		{
+			EmitSignal(SignalName.HolePunched);
+		}
+		
+		CloseClient();
+	}
+
+	public void HostRequest(string[] packet)
+	{
+		if (packet[1] == "OK")
+		{
+			GD.Print("Lobby Registered Created");
+			EmitSignal(SignalName.HostStarted);
+		}
+	}
+
+	public void HolePunch(string[] packet)
+	{
+		SendBytes("pp:Punching", new IPEndPoint(IPAddress.Parse(packet[1]), int.Parse(packet[2])));
+	}
+
+	public void SendBytes(string data, IPEndPoint endpoint)
+	{
+		byte[] byteArray = Encoding.UTF8.GetBytes(data);
+		Console.WriteLine($"Sending data to: {endpoint.ToString()}");
+		client.Send(byteArray, endpoint);
+	}
+
+	public void CloseClient()
+	{
+		client.Close();
+		_clientRunning = false;
+	}
 }
 
 
--- Scripts/MultiplayerController.cs
+++ Scripts/MultiplayerController.cs
@@ -4,175 +4,177 @@ using System.Linq;
 
 public partial class MultiplayerController : Control
 {
-    [Export]
-    private int port = 25565;
+	[Export]
+	private int port = 25565;
 
-    [Export]
-    private string ipAddress = "127.0.0.1";
+	[Export]
+	private string ipAddress = "127.0.0.1";
 
-    private ENetMultiplayerPeer peer;
+	private ENetMultiplayerPeer peer;
 
-    public override void _Ready()
-    {
-        GetNode<HolePunchingClient>("Hole Punching Component").HolePunched += joinGame;
-        Multiplayer.PeerConnected += PeerConnected;
-        Multiplayer.PeerDisconnected += PeerDisconnected;
-        Multiplayer.ConnectedToServer += ConnectedToServer;
-        Multiplayer.ConnectionFailed += ConnectionFailed;
-        if (OS.GetCmdlineArgs().Contains("--server"))
-        {
-            hostGame();
-        }
-    }
+	public override void _Ready()
+	{
+		GetNode<HolePunchingClient>("Hole Punching Component").HolePunched += joinGame;
+		GetNode<HolePunchingClient>("Hole Punching Component").HostStarted += hostGame;
+		Multiplayer.PeerConnected += PeerConnected;
+		Multiplayer.PeerDisconnected += PeerDisconnected;
+		Multiplayer.ConnectedToServer += ConnectedToServer;
+		Multiplayer.ConnectionFailed += ConnectionFailed;
+		if (OS.GetCmdlineArgs().Contains("--server"))
+		{
+			hostGame();
+		}
+	}
 
-    /// <summary>
-    /// Runs only on clients when connection failed
-    /// </summary>
-    /// <exception cref="NotImplementedException"></exception>
-    private void ConnectionFailed()
-    {
-        GD.Print("Connection Failed!");
-    }
+	/// <summary>
+	/// Runs only on clients when connection failed
+	/// </summary>
+	/// <exception cref="NotImplementedException"></exception>
+	private void ConnectionFailed()
+	{
+		GD.Print("Connection Failed!");
+	}
 
-    /// <summary>
-    /// Runs only on client when connection is successful
-    /// </summary>
-    /// <exception cref="NotImplementedException"></exception>
-    private void ConnectedToServer()
-    {
-        GD.Print("Connected to server");
-        RpcId(1, nameof(sendPlayerInfo), GetNode<LineEdit>("UI/NameInput").Text, Multiplayer.GetUniqueId());
-    }
+	/// <summary>
+	/// Runs only on client when connection is successful
+	/// </summary>
+	/// <exception cref="NotImplementedException"></exception>
+	private void ConnectedToServer()
+	{
+		GD.Print("Connected to server");
+		RpcId(1, nameof(sendPlayerInfo), GetNode<LineEdit>("UI/NameInput").Text, Multiplayer.GetUniqueId());
+	}
 
-    /// <summary>
-    /// Run on all peers when a player disconnects
-    /// </summary>
-    /// <param name="id">id of the player that disconnected</param>
-    /// <exception cref="NotImplementedException"></exception>
-    private void PeerDisconnected(long id)
-    {
-        GD.Print("Player Disconnected: " + id.ToString());
-        GameManager.Players.Remove(GameManager.Players.Where(i => i.Id == id).First<PlayerInfo>());
-        var players = GetTree().GetNodesInGroup("Player");
-        foreach(var player in players)
-        {
-            if(player.Name == id.ToString())
-            {
-                //player.QueueFree();
-            }
-        }
-    }
+	/// <summary>
+	/// Run on all peers when a player disconnects
+	/// </summary>
+	/// <param name="id">id of the player that disconnected</param>
+	/// <exception cref="NotImplementedException"></exception>
+	private void PeerDisconnected(long id)
+	{
+		GD.Print("Player Disconnected: " + id.ToString());
+		GameManager.Players.Remove(GameManager.Players.Where(i => i.Id == id).First<PlayerInfo>());
+		var players = GetTree().GetNodesInGroup("Player");
+		foreach(var player in players)
+		{
+			if(player.Name == id.ToString())
+			{
+				//player.QueueFree();
+			}
+		}
+	}
 
-    /// <summary>
-    /// Runs on all peers when a player connects
-    /// </summary>
-    /// <param name="id">id of the player that connected</param>
-    /// <exception cref="NotImplementedException"></exception>
-    private void PeerConnected(long id)
-    {
-        GD.Print("Player Connected: " + id.ToString());
-    }
+	/// <summary>
+	/// Runs on all peers when a player connects
+	/// </summary>
+	/// <param name="id">id of the player that connected</param>
+	/// <exception cref="NotImplementedException"></exception>
+	private void PeerConnected(long id)
+	{
+		GD.Print("Player Connected: " + id.ToString());
+	}
 
-    private void hostGame()
-    {
-        GetNode<HolePunchingClient>("Hole Punching Component").StartClient(true);
+	private void hostGame()
+	{
+		
 
-        GD.Print("Creating Server...");
-        peer = new ENetMultiplayerPeer();
-        var error = peer.CreateServer(port, 10);
-        if (error != Error.Ok)
-        {
-            GD.Print("Error failed to create server: " + error);
-            return;
-        }
-        peer.Host.Compress(ENetConnection.CompressionMode.RangeCoder);
+		GD.Print("Creating Server...");
+		peer = new ENetMultiplayerPeer();
+		var error = peer.CreateServer(port, 10);
+		if (error != Error.Ok)
+		{
+			GD.Print("Error failed to create server: " + error);
+			return;
+		}
+		peer.Host.Compress(ENetConnection.CompressionMode.RangeCoder);
 
-        Multiplayer.MultiplayerPeer = peer;
-        GD.Print("Server Successfully Created!\nWaiting For Players!");
+		Multiplayer.MultiplayerPeer = peer;
+		GD.Print("Server Successfully Created!\nWaiting For Players!");
 
-        StartGame();
-    }
+		StartGame();
+		sendPlayerInfo(GetNode<LineEdit>("UI/NameInput").Text, 1);
+	}
 
-    public void _on_host_button_down()
-    {
-        hostGame();   
-        sendPlayerInfo(GetNode<LineEdit>("UI/NameInput").Text, 1);
-    }
+	public void _on_host_button_down()
+	{
+		GetNode<HolePunchingClient>("Hole Punching Component").StartClient(true); 
+		
+	}
 
-    public void _on_join_button_down()
-    {
-        GetNode<HolePunchingClient>("Hole Punching Component").StartClient(false);
-    }
+	public void _on_join_button_down()
+	{
+		GetNode<HolePunchingClient>("Hole Punching Component").StartClient(false);
+	}
 
-    public void joinGame()
-    {
-        GD.Print("Connecting to Server...");
-        //ipAddress = GetNode<LineEdit>("IP Input").Text;
-        peer = new ENetMultiplayerPeer();
-        var error = peer.CreateClient(ipAddress, port);
-        if (error != Error.Ok)
-        {
-            GD.Print("Error failed to connect to server: " + error);
-            return;
-        }
-        peer.Host.Compress(ENetConnection.CompressionMode.RangeCoder);
-        Multiplayer.MultiplayerPeer = peer;
-        GD.Print("Successfully Connected to Server!");
-        // Load correct scene
-        StartGame();
-    }
+	public void joinGame()
+	{
+		GD.Print("Connecting to Server...");
+		//ipAddress = GetNode<LineEdit>("IP Input").Text;
+		peer = new ENetMultiplayerPeer();
+		var error = peer.CreateClient(ipAddress, port);
+		if (error != Error.Ok)
+		{
+			GD.Print("Error failed to connect to server: " + error);
+			return;
+		}
+		peer.Host.Compress(ENetConnection.CompressionMode.RangeCoder);
+		Multiplayer.MultiplayerPeer = peer;
+		GD.Print("Successfully Connected to Server!");
+		// Load correct scene
+		StartGame();
+	}
 
-    [Rpc(MultiplayerApi.RpcMode.AnyPeer)]
-    private void sendPlayerInfo(string name, int id)
-    {
-        PlayerInfo playerInfo = new PlayerInfo()
-        {
-            Name = name,
-            Id = id
-        };
-        if (GameManager.Players.Where(i => i.Id == id).Count() == 0) 
-        {
-            GameManager.Instance.AddPlayer(playerInfo);
-        }
-        if (Multiplayer.IsServer())
-        {
-            GameManager.Instance.ReadyClient();
-            foreach(var player in GameManager.Players)
-            {
-                Rpc(nameof(sendPlayerInfo), player.Name, player.Id);
-            }
-            RpcId(id, nameof(readyClient));
-        }
-    }
+	[Rpc(MultiplayerApi.RpcMode.AnyPeer)]
+	private void sendPlayerInfo(string name, int id)
+	{
+		PlayerInfo playerInfo = new PlayerInfo()
+		{
+			Name = name,
+			Id = id
+		};
+		if (GameManager.Players.Where(i => i.Id == id).Count() == 0) 
+		{
+			GameManager.Instance.AddPlayer(playerInfo);
+		}
+		if (Multiplayer.IsServer())
+		{
+			GameManager.Instance.ReadyClient();
+			foreach(var player in GameManager.Players)
+			{
+				Rpc(nameof(sendPlayerInfo), player.Name, player.Id);
+			}
+			//RpcId(id, nameof(readyClient));
+		}
+	}
 
-    [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = false)]
-    private void readyClient()
-    {
-        GameManager.Instance.ReadyClient();
-    }
+	[Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = false)]
+	private void readyClient()
+	{
+		GameManager.Instance.ReadyClient();
+	}
 
 
-    private void StartGame()
-    {
-        GetNode<Control>("UI").Hide();
-        GetTree().Paused = false;
+	private void StartGame()
+	{
+		GetNode<Control>("UI").Hide();
+		GetTree().Paused = false;
 
-        if (Multiplayer.IsServer())
-        {
-            CallDeferred(nameof(LoadScene), ResourceLoader.Load<PackedScene>("res://Scenes/Lobby.tscn"));
-        }
-    }
+		if (Multiplayer.IsServer())
+		{
+			CallDeferred(nameof(LoadScene), ResourceLoader.Load<PackedScene>("res://Scenes/Lobby.tscn"));
+		}
+	}
 
-    public void LoadScene(PackedScene scene)
-    {
-        Node prevScene = GetNode<Node>("Multiplayer/Scene");
+	public void LoadScene(PackedScene scene)
+	{
+		Node prevScene = GetNode<Node>("Multiplayer/Scene");
 
-        foreach(var child in prevScene.GetChildren())
-        {
-            prevScene.RemoveChild(child);
-            child.QueueFree();
-        }
+		foreach(var child in prevScene.GetChildren())
+		{
+			prevScene.RemoveChild(child);
+			child.QueueFree();
+		}
 
-        prevScene.AddChild(scene.Instantiate());
-    }
+		prevScene.AddChild(scene.Instantiate());
+	}
 }
